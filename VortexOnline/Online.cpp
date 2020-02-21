68f39a2177bc7ceed293efb2ccc004770eca5862
Axel Castells Monllau <axelcastells.dev@gmail.com>
2020-02-21 15:28:26 +0100

    Bug with duplicated connections on peers

--- VortexOnline/Online.cpp
+++ VortexOnline/Online.cpp
@@ -317,7 +317,6 @@ void TCP::Peer::ManagePeers()
 
 					int usersNum;
 					packet >> usersNum;
-					packet >> usersNum;
 					std::cout << usersNum << std::endl;
 
 					//std::vector<std::pair<std::string, short>> directions;
@@ -465,15 +464,19 @@ void TCP::BootstrapServer::ManageSockets()
 				if (sockets.size() >= maxUsers) {
 
 					sf::Packet pack;
-					pack << sockets.size();
+					pack << (int)sockets.size();
 					std::cout << sockets.size() << std::endl;
-					for (int i = 0; i < sockets.size() - 1; i++) {
+					for (int i = 0; i < sockets.size(); i++) {
 						std::cout << "Adding socket data: " << "IP: " << sockets[i]->getRemoteAddress() << "SERVER_PORT: " << sockets[i]->getRemotePort() << std::endl;
 						pack << sockets[i]->getRemoteAddress().toString() << sockets[i]->getRemotePort();
 
 
 					}
-					sockets[sockets.size() - 1]->send(pack);
+
+					for (int i = 0; i < sockets.size(); i++) {
+						sockets[i]->send(pack);
+					}
+					//sockets[sockets.size() - 1]->send(pack);
 
 					// ------
 
